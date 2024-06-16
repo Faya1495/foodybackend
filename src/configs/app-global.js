54d4b8f2e1090e82a53f783af364edaee5abb1a2
Faya1495 <faye43480@gmail.com>
2024-06-17 02:18:19 +0300

    first

---
+++ src/configs/app-global.js
@@ -0,0 +1,39 @@
+export const PROJECT_NAME = 'Foodyman marketplace';
+export const BASE_URL = 'https://cultbackend.pixer.tech';
+export const WEBSITE_URL = 'https://foody.pixer.tech';
+export const api_url = BASE_URL + '/api/v1/';
+export const api_url_admin = BASE_URL + '/api/v1/dashboard/admin/';
+export const api_url_admin_dashboard = BASE_URL + '/api/v1/dashboard/';
+export const IMG_URL = '';
+export const MAP_API_KEY = 'AIzaSyCKl1KzUQUq6m-5a_Wx60MyU6M8ton1oEc';
+export const export_url = BASE_URL + '/storage/';
+export const example = BASE_URL + '/';
+export const defaultCenter = { lat: 40.7127281, lng: -74.0060152 };
+
+export const VAPID_KEY =
+  'BNlT_lsM1CqdCsTP9PPngS8nDUv2yDpCKCeuruNsZ9Sa5ePSos-JhzV_dZ8ejHItLLfXEp4eCuBBa8cZRSBf8iE';
+export const API_KEY = 'AIzaSyD4I6XwPLliKVsY05AbbIm6M-2-4zSQa_o';
+export const AUTH_DOMAIN = 'file-22e2f.firebaseapp.com';
+export const PROJECT_ID = 'file-22e2f';
+export const STORAGE_BUCKET = 'file-22e2f.appspot.com';
+export const MESSAGING_SENDER_ID = '65637202910';
+export const APP_ID = '1:65637202910:web:39c9c9e63de1eb5c177b4f';
+export const MEASUREMENT_ID = 'G-K9DKK9T44N';
+
+export const RECAPTCHASITEKEY = '6LdnGc0pAAAAAEFOiiKBYEODLHUW8RquAM8y2sNx';
+
+export const DEMO_SELLER = 107; // seller_id
+export const DEMO_SHOP = 501; // seller_id
+export const DEMO_DELIVERYMAN = 106; // deliveryman_id
+export const DEMO_MANEGER = 114; // maneger_id
+export const DEMO_MODERATOR = 297; // moderator_id
+export const DEMO_ADMIN = 501; // administrator_id
+export const DEMO_WAITER = 108; // waiter_id
+
+export const SUPPORTED_FORMATS = [
+  'image/jpg',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'image/svg',
+];
